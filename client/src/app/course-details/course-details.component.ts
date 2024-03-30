a4b315910bb8bf85cad5bf6f247340ad9992f84f
Gaddour <mannai.abdlkader@gmail.com>
2024-03-30 13:41:39 +0100

    init course-details component

---
+++ client/src/app/course-details/course-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'course-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-details.component.html',
+  styleUrl: './course-details.component.css',
+})
+export class CourseDetailsComponent {
+
+  data: any = {};
+}
