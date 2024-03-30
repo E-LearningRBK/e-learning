d7b1c4cb90cadb13d18a376ec1cb7f3435387a06
Gaddour <mannai.abdlkader@gmail.com>
2024-03-30 14:51:06 +0100

    init styling course-details

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { CourseDetailsComponent } from './app/course-details/course-details.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(CourseDetailsComponent, appConfig)
   .catch((err) => console.error(err));
