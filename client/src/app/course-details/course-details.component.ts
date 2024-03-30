fd2a23bf9dcf583372d7042c2cda805fa67e4a5e
Gaddour <mannai.abdlkader@gmail.com>
2024-03-30 13:42:58 +0100

    fetch one course data

--- client/src/app/course-details/course-details.component.ts
+++ client/src/app/course-details/course-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'course-details',
@@ -7,7 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './course-details.component.html',
   styleUrl: './course-details.component.css',
 })
-export class CourseDetailsComponent {
+export class CourseDetailsComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
   data: any = {};
+
+  ngOnInit() {
+    this.http
+      .get('http://127.0.0.1:3000/api/material/getOne/5')
+      .subscribe((data) => {
+        console.log(data);
+        
+        this.data = data;
+      });
+  }
 }
