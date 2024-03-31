966898b57c995c45c77d5553061e56bb72f72207
lindamnasri <mnasrilinda20@gmail.com>
2024-03-31 22:46:19 +0100

    add register route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginComponent } from './login/login.component';
 import { CourseDetailsComponent } from './course-details/course-details.component';
 import { UserCoursesComponent } from './user-courses/user-courses.component';
 import { courses } from './courses/courses.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'register', component:RegisterComponent },
   { path: 'course', component: CourseDetailsComponent },
   { path: 'my-courses', component: UserCoursesComponent },
   { path: 'all-courses', component: courses },
