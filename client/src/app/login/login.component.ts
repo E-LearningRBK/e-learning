cdd58ffc491bd8d74622d27e6aff8aaf1216ea0c
lindamnasri <mnasrilinda20@gmail.com>
2024-03-31 01:03:36 +0100

    setting login functionality

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { AuthService } from '../Services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private authService: AuthService) { }
 
+  form: any = {
+    email: null,
+    password: null,
+  };
+ 
+  // errorMessage: string | null = null;
+ 
+
+  onSubmit(): void {
+    const { email, password } = this.form;
+
+    this.authService.login(email, password).subscribe({
+      next: (res) => {
+        console.log(res);
+        localStorage.setItem('token', res.token)
+        // this.router.navigate([]);
+      },
+      error: (error) => {
+        console.log('User not found.');
+      },
+    });
+  }
 }
