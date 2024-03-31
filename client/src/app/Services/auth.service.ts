134b31640041c491480b0eb24e5eb4dcdc23bf6c
lindamnasri <mnasrilinda20@gmail.com>
2024-03-31 01:01:28 +0100

    setting the login  auth services

---
+++ client/src/app/Services/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post('http://127.0.0.1:3000/api/users/signin', {
+      email,
+      password,
+    });
+  }
+}
