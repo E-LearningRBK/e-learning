3a745bbcf5ab5c656e0b7323594c5fdd62ff65a0
lindamnasri <mnasrilinda20@gmail.com>
2024-03-31 14:09:53 +0100

    init singup functionnality

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../Services/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  form: any = {
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String,
+    role: String,
+    imageUrl: String,
+  };
+  constructor(private authService: AuthService) {}
 
+  onSubmit(): void {
+    const { firstname, lastname, email, password, role, imageUrl } = this.form;
+
+    this.authService
+      .singup(firstname, lastname, email, password, role, imageUrl)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
 }
