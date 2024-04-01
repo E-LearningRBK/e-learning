bf7d8832b86aa46413cc161afbd325cf90bd91f0
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-04-01 14:47:14 +0100

    setting the back for the sign up

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,32 +2,36 @@ const bcrypt = require("bcrypt");
 const User = require("../model/user.model.js");
 const jwt = require("jsonwebtoken");
 
-const { upload,getRandomString } = require("../helper/helperFunction.js");
-const { sendConfirmation } = require("../utils/sendEmail.js");
-
-
+const { upload, getRandomString } = require("../helper/helperFunction.js");
+// const { sendConfirmation } = require("../utils/sendEmail.js");
 
 const signup = async (req, res) => {
-  const { email, firstName, lastName, password, role, imageUrl } = req.body;
+  console.log(req.body);
+  console.log(req.files[0]);
+  const { email, firstName, lastName, password } = req.body;
 
-  // This will log a random string of 8 characters
+                               // This will generate a random string of 8 characters
+
   const randomString = getRandomString(8);
-  // getting the data
+
+                               // getting the data
 
   try {
-    //checking if the email is already in use
+                               //checking if the email is already in use
+
     const checkemail = await User.findOne({ where: { email: email } });
     if (checkemail) {
       return res.status(400).json({ error: "existing account  " });
     }
-    //hashing the password
+                               //hashing the password
     let hashedpass = await bcrypt.hash(password, 10);
-    //creating the new user
-    // const imageBuffer = req.files[0].buffer;
-    // const imageUrl = await upload(imageBuffer);
+                                //creating the new user
+    const imageBuffer = req.files[0].buffer;
+    const imageUrl = await upload(imageBuffer);
+    console.log(imageUrl);
     const user = await User.create({
       firstName: firstName,
-      imageUrl: "",
+      imageUrl: imageUrl,
       lastName: lastName,
       email: email,
       password: hashedpass,
@@ -46,18 +50,18 @@ const signup = async (req, res) => {
 };
 const signin = async (req, res) => {
   try {
-    // cheking if the email and password are not a falsy value
+                            // cheking if the email and password are not a falsy value
     const { email, password } = req.body;
     if (!email || !password) {
       return res.status(404).json({ error: "Email or Password not found." });
     }
-    //cheking if the email exist in the database
-    const user = await User.findOne({ where: { email:email } });
+                             //cheking if the email exist in the database
+    const user = await User.findOne({ where: { email: email } });
 
     if (!user) {
       return res.status(400).json({ error: "User not found." });
     }
-    //cheking if the password is valid
+                              //cheking if the password is valid
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordMatch) {
@@ -66,7 +70,7 @@ const signin = async (req, res) => {
     if (user.isactive == false) {
       return res.status(401).json({ error: "avtivate your account" });
     }
-    // Generate a JSON Web Token (JWT) for authentication
+                                 // Generate a JSON Web Token (JWT) for authentication
     const token = jwt.sign(
       {
         userId: user.id,
@@ -77,13 +81,21 @@ const signin = async (req, res) => {
         expiresIn: "1d",
       }
     );
-   
-    res.status(200).json({token, message: "succeeded" });
+                              //sending a succeeded response
+
+    res.status(200).json({ token, message: "succeeded" });
+
+                              //sending a error response
+
   } catch (error) {
+
     console.error(error);
     res.status(500).send(error);
   }
 };
+
+                              //getting all users
+
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.findAll({ where: { role: "student" } });
@@ -93,17 +105,23 @@ const getAllUsers = async (req, res) => {
     res.status(500).send(error);
   }
 };
+                              //getting one user
+
 const getOne = async (req, res) => {
   try {
-    const user = await User.findOne({ where: { id: req.userId } });
+    const user = await User.findByPk(req.userId );
     res.status(200).send(user);
   } catch (err) {
     res.status(500).send(err);
   }
 };
+
+                              //updating one user
+
 const updateUser = async (req, res) => {
   try {
-    const { firstName, lastName, imageUrl, currentPassword, newPassword } =req.body;
+    const { firstName, lastName, imageUrl, currentPassword, newPassword } =
+      req.body;
     let user = await User.findByPk(req.userId);
 
     if (!user) {
@@ -119,7 +137,10 @@ const updateUser = async (req, res) => {
     const url = await upload(imageBuffer);
     user.imageUrl = url;
     if (currentPassword && newPassword) {
-      const passwordMatch = await bcrypt.compare( currentPassword,user.password);
+      const passwordMatch = await bcrypt.compare(
+        currentPassword,
+        user.password
+      );
       if (!passwordMatch) {
         return res
           .status(401)
@@ -135,6 +156,8 @@ const updateUser = async (req, res) => {
     res.status(500).send(error);
   }
 };
+
+                    // user verification throw  email
 const verifyUser = async (req, res) => {
   try {
     const user = await User.findOne({
