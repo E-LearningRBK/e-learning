c84537d19d7c71c97bee3686b2c3b3e3281241ba
ahmedGDI <guedria.guedri@gmail.com>
2024-04-03 14:51:28 +0100

    handle update in UpdateCourseForAdminComponent

--- client/src/app/admin/components/update-course-for-admin/update-course-for-admin.component.ts
+++ client/src/app/admin/components/update-course-for-admin/update-course-for-admin.component.ts
@@ -1,28 +1,58 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AdminMaterialServiceService } from '../../service/admin-material-service.service';
+import { Material } from '../../model/courses.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-update-course-for-admin',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './update-course-for-admin.component.html',
   styleUrl: './update-course-for-admin.component.css'
 })
 export class UpdateCourseForAdminComponent {
   constructor(
     private AdminMaterialServiceService :  AdminMaterialServiceService ,
-    private router : Router
+    private router : Router,
+    private active : ActivatedRoute
     ){}
-  
-    updateMat(): void {
-  this.AdminMaterialServiceService.updateMat('implement here').subscribe()
+    form: Material = {
+      name: '', 
+      description: '', 
+      price: 0, 
+      date: '', 
+      link: '' 
+    };
+
+id:any
+navigateToAllCourses():void{
+this.router.navigate(['/admin/AllCoursesForAdmin'])
+}
+    update(): void {
+      this.active.paramMap.subscribe((param)=>{
+            this.id = param.get('id') 
+            console.log("this.id :",this.id )
+            this.AdminMaterialServiceService.updateMat(this.id, this.form).subscribe(
+              () => {
+                console.log('updated !');
+                this.navigateToAllCourses()
+              },
+              () => {
+                
+              
+                alert('implement all the feilds');
+              }
+              
+            );
+      })
     }
-  
+
     
-    navigateToAllCourses():void{
-      this.router.navigate(['/admin/AllCoursesForAdmin'])
-    }
+    
+    
+   
+    
 }
 
 
