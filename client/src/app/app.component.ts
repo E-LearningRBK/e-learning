da9f7700da8aca37597456ea5fdd8c86a4cbd1d4
Gaddour <mannai.abdlkader@gmail.com>
2024-04-03 05:43:47 +0100

    Admin Index Component and its children routes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component'; //navbar user
-import { navBarComponent } from './admin/components/nav-bar/nav-bar.component'; //navbar admin
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,navBarComponent,NavBarComponent],
+  imports: [RouterOutlet,NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
