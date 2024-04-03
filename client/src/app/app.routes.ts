e0b4238a669c8f7574e8e661de51061583811583
Gaddour <mannai.abdlkader@gmail.com>
2024-04-03 14:49:25 +0100

    Organize Routes And exclude welcome page from authGuard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -18,6 +18,29 @@ import { IndexComponent } from './student/components/index/index.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
+  // Welcome Route
+  { path: '', component: WelcomeComponent },
+  // User Routes
+  {
+    path: '',
+    component: IndexComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: 'home', component: HomeComponent }, // "HomeComponent" user for user
+      {
+        path: 'course',
+        component: CourseDetailsComponent,
+      },
+      { path: 'course/:id', component: CourseDetailsComponent },
+      { path: 'my-courses', component: UserCoursesComponent },
+      { path: 'edit', component: EditComponent },
+      { path: 'all-courses', component: courses },
+    ],
+  },
+  // Auth Routes
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  // Admin Routes
   {
     path: 'admin',
     component: AdminIndexComponent,
@@ -30,23 +53,4 @@ export const routes: Routes = [
       { path: 'studentDetail/:id', component: StudentDetailComponent },
     ],
   },
-  {
-    path: '',
-    component: IndexComponent,
-    canActivate: [authGuard],
-    children: [
-      { path: '', component: WelcomeComponent },
-      { path: 'home', component: HomeComponent }, // "HomeComponent" user for user
-      {
-        path: 'course',
-        component: CourseDetailsComponent,
-      },
-      { path: 'course/:id', component: CourseDetailsComponent },
-      { path: 'my-courses', component: UserCoursesComponent },
-      { path: 'edit', component: EditComponent },
-      { path: 'all-courses', component: courses },
-    ],
-  },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
 ];
