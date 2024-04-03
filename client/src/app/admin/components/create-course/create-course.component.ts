ebe07878e3d8327687f31f61144d40604f8d0e04
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-04-03 10:42:51 +0100

    creating the create course copmponent

--- client/src/app/admin/components/create-course/create-course.component.ts
+++ client/src/app/admin/components/create-course/create-course.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import{MatService}from '../../service/mat.service'
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-create-course',
+  selector: 'create-course',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-course.component.html',
   styleUrl: './create-course.component.css'
 })
 export class CreateCourseComponent {
+  constructor(private MatService:MatService,private router: Router){}
+  form: any = {
+    name: null,
+    description: null,
+    price: null,
+    link: null,
+    date: null
+  };
+  isModalOpen = false;
+  toggleModal() {
+    this.isModalOpen = !this.isModalOpen;
+  }
+  onSubmit(): void {
+      this.MatService.createMat(this.form).subscribe((res) => {
+        if(res){
+          this.toggleModal()
+          alert('created sucssefully')
+          this.router.navigate(['/AllCoursesForAdmin']);
 
+        };
+        
+      });
+    
+  }
 }
