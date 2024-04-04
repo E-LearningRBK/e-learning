1f0e496025d3c92d02090dc726871f7afa7ad067
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-04-04 23:05:47 +0100

    setting the  service payment

--- client/src/app/student/components/courses/courses.service.ts
+++ client/src/app/student/components/courses/courses.service.ts
@@ -5,22 +5,20 @@ import { Material } from './courses.model';
 
 @Injectable({
   providedIn: 'root',
-
 })
 export class MaterialService {
   private baseUrl = 'http://127.0.0.1:3000/api/material';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAllMaterials(): Observable<Material[]> {
     return this.http.get<Material[]>(`${this.baseUrl}/getAll`);
   }
 
-  // getMaterialById(id: number): Observable<Material> {
-  //   return this.http.get<Material>(`${this.baseUrl}/getOne/${id}`);
-  // }
-  paymentDirection(material:any): Observable<any>  {
-    return this.http.post(`http://127.0.0.1:3000/api/payment/add`,material.price);
+  getMaterialById(id: number): Observable<Material> {
+    return this.http.get<Material>(`${this.baseUrl}/getOne/${id}`);
   }
-
+  paymentDirection(material: any): Observable<any> {
+    return this.http.post(`http://127.0.0.1:3000/api/payment/add`, material);
+  }
 }
