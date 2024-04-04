1f665a04621a92fd2eb6c6373ac0f79a8bb8ba3a
ahmedGDI <guedria.guedri@gmail.com>
2024-04-04 13:01:02 +0100

    add pagination for allUsers comp in admin too

--- client/src/app/admin/components/all-users-for-admin/all-users-for-admin.component.ts
+++ client/src/app/admin/components/all-users-for-admin/all-users-for-admin.component.ts
@@ -11,6 +11,9 @@ import { Router } from '@angular/router';
 })
 export class AllUsersForAdminComponent implements OnInit {
   users: users[] = [];
+  pageSize: number = 10;
+  currentPage: number = 1;
+  totalPages: number = 1;
 
   constructor(
     private AdminUserServiceService: AdminUserServiceService,
@@ -24,6 +27,7 @@ export class AllUsersForAdminComponent implements OnInit {
   fetcher(): void {
     this.AdminUserServiceService.getAllUsers().subscribe((users) => {
       this.users = users;
+      this.totalPages = Math.ceil(this.users.length / this.pageSize);
     });
   }
 
@@ -31,5 +35,14 @@ export class AllUsersForAdminComponent implements OnInit {
     this.router.navigate(['/admin/studentDetail', user.id]);
   }
 
+  onPageChange(page: number): void {
+    this.currentPage = page;
+  }
+
+  getpagUser(): users[] {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.users.slice(startIndex, endIndex);
+  }
   
 }
