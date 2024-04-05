e532170960d2a17caeb480624fc7d113842f9baa
ahmedGDI <guedria.guedri@gmail.com>
2024-04-05 03:31:40 +0100

    modify AllUsersForAdminComponent too

--- client/src/app/admin/components/all-users-for-admin/all-users-for-admin.component.ts
+++ client/src/app/admin/components/all-users-for-admin/all-users-for-admin.component.ts
@@ -2,18 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { AdminUserServiceService } from '../../service/admin-user-service.service';
 import { users } from '../../model/users.model';
 import { Router } from '@angular/router';
+import { AdminPaginationForUsersComponent } from '../admin-pagination-for-users/admin-pagination-for-users.component';
+
 @Component({
   selector: 'app-all-users-for-admin',
   standalone: true,
-  imports: [],
+  imports: [AdminPaginationForUsersComponent],
   templateUrl: './all-users-for-admin.component.html',
   styleUrl: './all-users-for-admin.component.css'
 })
 export class AllUsersForAdminComponent implements OnInit {
   users: users[] = [];
-  pageSize: number = 10;
-  currentPage: number = 1;
-  totalPages: number = 1;
+ 
 
   constructor(
     private AdminUserServiceService: AdminUserServiceService,
@@ -27,7 +27,6 @@ export class AllUsersForAdminComponent implements OnInit {
   fetcher(): void {
     this.AdminUserServiceService.getAllUsers().subscribe((users) => {
       this.users = users;
-      this.totalPages = Math.ceil(this.users.length / this.pageSize);
     });
   }
 
@@ -35,14 +34,5 @@ export class AllUsersForAdminComponent implements OnInit {
     this.router.navigate(['/admin/studentDetail', user.id]);
   }
 
-  onPageChange(page: number): void {
-    this.currentPage = page;
-  }
-
-  getpagUser(): users[] {
-    const startIndex = (this.currentPage - 1) * this.pageSize;
-    const endIndex = startIndex + this.pageSize;
-    return this.users.slice(startIndex, endIndex);
-  }
   
 }
